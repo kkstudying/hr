0fd3c0f4ed9b6a5bb00432787395d040c2a2024e
taotao <1183666701@qq.com>
2023-01-01 14:00:55 +0800

    userInfo

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,7 +2,8 @@ import { login } from '@/api/user'
 import { getToken } from '@/utils/auth'
 const state = {
   // 存放数据
-  token: getToken() || ''
+  token: getToken() || '',
+  userInfo: {}
 }
 const mutations = {
   // 修改数据
@@ -11,12 +12,18 @@ const mutations = {
     localStorage.setItem('token', newToken)
     // setToken(newToken)
     console.log(newToken)
+  },
+  setUserInfo(state, newUserInfo) {
+    state.userInfo = newUserInfo
   }
 }
 const actions = {
   async login(store, loginForm) {
     const res = await login(loginForm)
     store.commit('setToken', res)
+  },
+  getUserInfo(store){
+    
   }
 }
 
